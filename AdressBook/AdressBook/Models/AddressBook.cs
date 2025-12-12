3d22c35ba1894ffce1c6eac8e1037bb1e25a201d
Karthikkm1925 <Karthikkm1925@gmail.com>
2025-12-12 15:30:58 +0530

    UC1 first commit

---
+++ AdressBook/AdressBook/Models/AddressBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem.Models
+{
+    public class AddressBook
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public List<Contact> GetAllContacts()
+        {
+            return contacts;
+        }
+    }
+}
--- AdressBook/AdressBook/Program.cs
+++ AdressBook/AdressBook/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace AddressBook
+﻿using System;
+using AddressBookSystem.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To AddressBookSystem !!");
+            AddressBook addressBook = new AddressBook();
+            Console.WriteLine("Welcome to Address Book Program!");
+
+            Console.WriteLine("Enter First Name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address:");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter City:");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter State:");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip:");
+            string zip = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone:");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("Enter Email:");
+            string email = Console.ReadLine();
+
+            Contact c = new Contact
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip,
+                Phone = phone,
+                Email = email
+            };
+
+            addressBook.AddContact(c);
+
+            Console.WriteLine("\nContact Added Successfully!\n");
+            Console.WriteLine(c);
+
+
         }
     }
 }
